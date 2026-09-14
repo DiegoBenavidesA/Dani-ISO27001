c6a8b9f4f49dc9a27ab7952347752b83fdbe2714
celinealcaino22 <celinealcaino22@gmail.com>
2026-09-13 23:01:39 -0300

    feat: registrar modelos Ley 21719

--- Producto/dani-project-back/app/models/__init__.py
+++ Producto/dani-project-back/app/models/__init__.py
@@ -11,7 +11,7 @@ from app.models.normative_chunk import NormativeChunk
 from app.models.document import Document, DocumentStatus
 
 # --- Modelos Ley N° 21.719 (protección de datos personales) ---
-from app.models.data_treatment import DataTreatment, LegalBasis          # Tarea 1.1
+from app.models.data_treatment import DataTreatment                       # Tarea 1.1
 from app.models.vendor import Vendor, ContractStatus                     # Tarea 1.5
 from app.models.impact_assessment import ImpactAssessment, ImpactRiskLevel, ImpactStatus  # Tarea 1.6
 
@@ -24,7 +24,7 @@ __all__ = [
     "NormativeChunk",
     "Document", "DocumentStatus",
     # Ley 21.719
-    "DataTreatment", "LegalBasis",
+    "DataTreatment", 
     "Vendor", "ContractStatus",
     "ImpactAssessment", "ImpactRiskLevel", "ImpactStatus",
 ]
